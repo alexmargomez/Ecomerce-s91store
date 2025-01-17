ddb294b30b0f0d92c96d856625d3be2a3689a20f
alexmargomez <fabioalexmar@gmail.com>
2025-01-17 12:25:42 -0500

    integración de cors

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './src/routes/productRoutes.js';
+import cors from 'cors';
 
 const app= express();
 dotenv.config();
 app.use('/uploads', express.static('./src/uploads'));
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000', // Permite el frontend
+}));
 app.use('/',router);
 
 const port= process.env.PORT || 5000;
